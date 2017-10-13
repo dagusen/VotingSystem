680d6f377f77b8606aca63d135d5c5263dcf5cdf
Louis <louisaleksieb.dagusen@gmail.com>
2017-10-14 01:49:03 +0800

    fixed

--- Vote/urls.py
+++ Vote/urls.py
@@ -10,9 +10,9 @@ from PartyList.views import VotePartyListView
 
 urlpatterns = [
     url(r'^$', VotePartyListView.as_view(), name='list'),
-    # url(r'^pdp-laban/$', VotePDPLabanListView.as_view(), name='list'),
+    url(r'^pdp-laban/$', VotePDPLabanListView.as_view(), name='list'),
     # url(r'^yellow-ribbon/$', VoteYellowListView.as_view(), name='list'),
-    url(r'^(?P<slug>[\w-]+)/$', VotePDPLabanListView.as_view(), name='list'),
+    #url(r'^(?P<slug>[\w-]+)/$', VotePDPLabanListView.as_view(), name='list'),
     url(r'^(?P<slug>[\w-]+)/$', VoteYellowListView.as_view(), name='list'),
     #url(r'^create/$', PartyListCreateView.as_view(), name='create'),
     # # slug
